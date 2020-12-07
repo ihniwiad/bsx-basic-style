7727ecc57e827000fc86c2b4fcc195793e293e07
ihniwiad <mail@matthiasbroecker.de>
2020-12-07 17:52:35 +0100

    UPDATE: added sameSite attribute

--- resources/components/js/scripts/utils/cookie-handler/js/cookie-handler.js
+++ resources/components/js/scripts/utils/cookie-handler/js/cookie-handler.js
@@ -2,10 +2,11 @@
 
 	var CookieHandler = {};
 
-	CookieHandler.setCookie = function( cookieName, cookieValue, expiresDays, path ) {
+	CookieHandler.setCookie = function( cookieName, cookieValue, expiresDays, path, sameSite ) {
 	    var date = new Date();
+	    var sameSiteDefault = 'strict';
 	    date.setTime( date.getTime() + ( expiresDays * 24 * 60 * 60 * 1000 ) );
-	    document.cookie = cookieName + '=' + cookieValue + '; ' + 'expires=' + date.toUTCString() + ( !! path ? '; path=' + path : '' );
+	    document.cookie = cookieName + '=' + cookieValue + '; ' + 'expires=' + date.toUTCString() + ( !! path ? '; path=' + path : '' ) + '; sameSite=' + ( !! sameSite ? sameSite : sameSiteDefault ) + ( sameSite == 'none' ? '; secure' : '' );
 	};
 
 	CookieHandler.getCookie = function( cookieName ) {
